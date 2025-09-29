97109bbac2e5fd151bbc8001b79622e97a645d1d
Metin Türk <metinturk553@gmail.com>
2025-09-29 14:45:39 +0300

    küçük fonks tekrarları yaptım.

--- Udemy/degiskenler/lib/fonksiyonlar/fonksiyonlar_main.dart
+++ Udemy/degiskenler/lib/fonksiyonlar/fonksiyonlar_main.dart
@@ -4,4 +4,14 @@ void main(){
   var fonksiyon = Fonksiyon();
 
   print("Sonuc: ${fonksiyon.faktoriyel(5)}");
+
+  print("Girdiğiniz kelimede : ${fonksiyon.harfSayac("emeklemek")} tane 'e' harfi vardır.");
+
+  var kenarSayisi = 5;
+  print("${kenarSayisi} kenarlı şekilin iç açıları toplamı: ${fonksiyon.aciToplami(kenarSayisi)}");
+
+  print("Maaşınız: ${fonksiyon.maasHesapla(10)}");
+
+  print("Otopark Ücretiniz: ${fonksiyon.otoparUcret(4)}");
+
 }
--- Udemy/degiskenler/lib/fonksiyonlar/fonksiyonlar_odev.dart
+++ Udemy/degiskenler/lib/fonksiyonlar/fonksiyonlar_odev.dart
@@ -10,4 +10,45 @@ class Fonksiyon{
     }
     return sonuc;
   }
+
+  int harfSayac(String kelime){
+    var sayac = 0;
+    for(int i = 0; i < kelime.length; i++){
+      if(kelime[i] == "e"){
+        sayac++;
+      }
+    }
+    return sayac;
+  }
+
+  double aciToplami(int kenarSayisi){
+    double toplam = ((kenarSayisi - 2)*180)/ kenarSayisi;
+    return toplam;
+  }
+
+  int maasHesapla(int calisilanGun){
+    var toplamSaat = calisilanGun * 8;
+    int mesaiSaat = 0;
+    if(toplamSaat >= 150){
+      mesaiSaat = toplamSaat - 150;
+      return (150*40) + (mesaiSaat * 80);
+    }
+    else{
+      return toplamSaat*40;
+    }
+  }
+  int otoparUcret(int kalinanSaat){
+    if(kalinanSaat <= 2){
+      return 100;
+    }
+    else{
+      var fazladanSaat = kalinanSaat - 2;
+      int fazlaUcret = 0, fazlaodenecek = 0;
+      for(int i = 0 ; i < fazladanSaat ; i++){
+        fazlaUcret += 10;
+        fazlaodenecek += 50+fazlaUcret;
+      }
+      return 100+fazlaodenecek;
+    }
+  }
 }
