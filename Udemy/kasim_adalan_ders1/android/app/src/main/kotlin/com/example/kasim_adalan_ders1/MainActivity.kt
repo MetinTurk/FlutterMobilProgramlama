995816299b943669872903f39129436c3e681616
Metin Türk <metinturk553@gmail.com>
2025-10-14 21:27:52 +0300

    Udemy üzerinden widget yapısını çalışmaya başladım.

---
+++ Udemy/kasim_adalan_ders1/android/app/src/main/kotlin/com/example/kasim_adalan_ders1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kasim_adalan_ders1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Udemy/kasim_adalan_ders1/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Baslik'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("Uygulama Açıldı");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: Text(widget.title),
+      ),
+      body:Row(children: [
+        Container(
+          margin: const EdgeInsets.only(top: 30.0,left: 5.00),
+          width: 200,
+          height: 200,
+
+          child: Text("Merhaba"),
+          decoration: BoxDecoration(
+            color: Colors.red,
+            border: Border.all(
+              color: Colors.blue,
+              width: 3.0,
+            ),
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+        ),
+      ]
+        ,)
+        
+        
+      
+      /*Stack(
+        children: [
+          Container(width: 400, height: 400,color: Colors.red),
+          Row(
+            children: [
+              Container(width: 100,height: 100,color: Colors.white),
+              Container(width: 100,height: 100,color: Colors.blue),
+              Column(
+                children: [
+                  Container(width: 100,height: 100,color: Colors.yellow),
+                  Container(width: 100,height: 100,color: Colors.green),
+                ],
+              )
+            ],
+          ),
+        ],
+      )
+*/
+
+      /*Stack(
+        children: [
+          Container(width: 100,height: 100,color: Colors.red),
+          Container(width: 50,height: 50,color: Colors.blue),
+          Container(width: 25,height: 25,color: Colors.green),
+          Text("Merhaba"),
+        ],
+      ),*/
+      /*Column(   //Sütun
+        children: [
+          Text("Merhaba"),
+          Container(width: 100,height: 100,color: Colors.red),
+          Container(width: 50,height: 50,color: Colors.blue),
+          Container(width: 25,height: 25,color: Colors.green),
+        ],
+      )*/
+      /*Row(    //Satır
+        children: [
+          Text("Merhaba"),
+          Container(width: 100,height: 100,color: Colors.red),
+          Container(width: 80,height: 80,color: Colors.blue),
+          Container(width: 25,height: 25,color: Colors.green),
+        ],
+      ),
+      */
+
+    );
+  }
+}
