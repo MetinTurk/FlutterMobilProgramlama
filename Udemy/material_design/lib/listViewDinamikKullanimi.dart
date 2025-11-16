ef15bccb5801c1cee1f2743bd6e7927f9bf4c403
Metin Türk <metinturk553@gmail.com>
2025-11-16 11:50:32 +0300

    dinamik listview yatayda kaydırma kodları.

--- Udemy/material_design/lib/listViewDinamikKullanimi.dart
+++ Udemy/material_design/lib/listViewDinamikKullanimi.dart
@@ -19,50 +19,54 @@ class _ListviewdinamikkullanimiState extends State<Listviewdinamikkullanimi> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("Dinamik ListView"),
       ),
-      body: ListView.builder(
-        itemCount: ulkeler.length,
-        itemBuilder: (context,indeks){
-          return GestureDetector(
-            onTap: (){
-              print("${ulkeler[indeks]} seçildi");
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Dinamiklistviewdetaysayfa(ulkeAdi: ulkeler[indeks]),));
-            },
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  height: 50,
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                        onTap: (){
-                          print("Text ile ${ulkeler[indeks]} seçildi");
-                        },
-                          child: Text("${ulkeler[indeks]}")
-                      ),
-                      Spacer(),
-                      PopupMenuButton(
-                        child: Icon(Icons.more_vert),
-                        itemBuilder: (context) => [
-                          PopupMenuItem(value: 1, child: Text("Sil"),),
-                          PopupMenuItem(value: 2, child: Text("Güncelle"),),
-                        ],
-                        onSelected: (menuItemValue){
-                          if(menuItemValue == 1){
-                            print("${ulkeler[indeks]} silindi");
-                          }
-                          if(menuItemValue == 2){
-                            print("${ulkeler[indeks]} güncellendi");
-                          }
-                        },
-                      ),
-                    ],
+      body: SizedBox(
+        height: 100,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: ulkeler.length,
+          itemBuilder: (context,indeks){
+            return GestureDetector(
+              onTap: (){
+                print("${ulkeler[indeks]} seçildi");
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Dinamiklistviewdetaysayfa(ulkeAdi: ulkeler[indeks]),));
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 100,
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: (){
+                            print("Text ile ${ulkeler[indeks]} seçildi");
+                          },
+                            child: Text("${ulkeler[indeks]}")
+                        ),
+                        Spacer(),
+                        PopupMenuButton(
+                          child: Icon(Icons.more_vert),
+                          itemBuilder: (context) => [
+                            PopupMenuItem(value: 1, child: Text("Sil"),),
+                            PopupMenuItem(value: 2, child: Text("Güncelle"),),
+                          ],
+                          onSelected: (menuItemValue){
+                            if(menuItemValue == 1){
+                              print("${ulkeler[indeks]} silindi");
+                            }
+                            if(menuItemValue == 2){
+                              print("${ulkeler[indeks]} güncellendi");
+                            }
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
 
     );
